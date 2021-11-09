2202bb4c76a362206d88f1e505eb4915ce5d5ade
Eliza Jeffries <epicean@gmail.com>
2021-11-09 09:55:19 -0800

    Done except for node js
    okay not actually done but the basics are almost done

--- jsonEditor.js
+++ jsonEditor.js
@@ -11,54 +11,74 @@ window.onload = function(){
 };
 function printRes()
 {
-var date = document.getElementById("date").value
-console.log(date)
+var date = parseInt(document.getElementById("date").value)
+//console.log(date)
 dateArray.push(date)
-console.log(dateArray)
+//console.log(dateArray)
 
 var event = document.getElementById("title").value
-console.log(event)
+//console.log(event)
 eventArray.push(event)
-console.log(eventArray)
+//console.log(eventArray)
 }
+
 var obj = 
 {
-    "version":105,  
-        "1": null,
-        "2": null,
-        "3": null,
-        "4": null,
-        "5": null,
-        "6": null,
-        "7": null,
-        "8": null,
-        "9": null,
-        "10": null,
-        "11": null,
-        "12": null,
-        "13": null,
-        "14": null,
-        "15": null,
-        "16": null,
-        "17": null,
-        "18": null,
-        "19": null,
-        "20": null,
-        "21": null,
-        "22": null,
-        "23": null,
-        "24": null,
-        "25": null,
-        "26": null,
-        "27": null,
-        "28": null,
-        "29": null,
-        "30": null,
-        "31": null,
-        "comment": "Each number is a day of the month, this document will be updated each month. LEAVE ANY DAY THAT IS BLANK AS NULL;",
-        "comment2":"aiden help version number is broken"
+    "version": null,  
+    "1": null,
+    "2": null,
+    "3": null,
+    "4": null,
+    "5": null,
+    "6": null,
+    "7": null,
+    "8": null,
+    "9": null,
+    "10": null,
+    "11": null,
+    "12": null,
+    "13": null,
+    "14": null,
+    "15": null,
+    "16": null,
+    "17": null,
+    "18": null,
+    "19": null,
+    "20": null,
+    "21": null,
+    "22": null,
+    "23": null,
+    "24": null,
+    "25": null,
+    "26": null,
+    "27": null,
+    "28": null,
+    "29": null,
+    "30": null,
+    "31": null,
+    "comment": "Each number is a day of the month, this document will be updated each month. LEAVE ANY DAY THAT IS BLANK AS NULL;",
+    "comment2":"aiden help version number is broken"
 }
 function createFile()
 {
-    
+    //obj["'version'"] = 100;
+    //console.log(obj);
+    for (var i = 1; i <= 31; i++)
+    {
+        var index = dateArray.indexOf(i);
+        if (index > -1)
+        {
+           obj[i] = eventArray[index] 
+           console.log("found")
+        }
+    }
+    console.log(obj);
+
+
+    var dictstring = JSON.stringify(obj);
+    var fs = require('fs');
+    fs.writeFile("events.json", dictstring, function(err, result) {
+    if(err) console.log('error', err);
+});
 }
+
